a49b2c2eeee6ad88f79175c76bb898b5d69effc0
Teslatron55 <samuel@johnstonshome.org>
2018-02-26 11:06:43 -0800

    Imported current progress

---
+++ Assets/DungeonModules/Scripts/DoorAOpen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAOpen : MonoBehaviour
+{
+    public Animator anim;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        // GetComponent<Animator>().SetTrigger("DoorATrigger");
+        anim.SetTrigger("DoorATrigger");
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        // GetComponent<Animator>().SetTrigger("DoorATrigger");
+        anim.speed = -1;
+        anim.SetTrigger("DoorATrigger");
+
+    }
+
+}
